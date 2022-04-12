056eae0a32210502e2107c7341e4545fa03dace2
kafan1986 <debasish1986@gmail.com>
2022-04-13 02:37:15 +0530

    Update Syllabification.py

--- src/Syllabification.py
+++ src/Syllabification.py
@@ -1,28 +1,18 @@
 # -*- coding: utf-8 -*-
-from tkinter import *
-from itertools import groupby
-import tkinter.filedialog
-import codecs
-import sys
-import re
-import os
+from tkinter import END
 from IPAEquv import IPAEquivalent
 from Labelchang1 import Labelchanger1
 from Labelchang2 import Labelchanger2
 from countvc import countvowel
-# import importlib
-# importlib.reload(sys)
-# #reload(sys)
-# sys.setdefaultencoding('utf-8')
 uvwl = {u'\u0259': 0, u'u': 0, u'E': 0, u'M': 0, u'A': 0, u'O': 0, u'\u0254': 0, "J": 0, "N": 0, u'i': 0, u'a': 0,
-        u'U': 0, u'I': 0, u'!': 0, u'#': 0, u'%': 0, u'(': 0, u')': 0, u'_': 0, u'+': 0, u'=': 0, u'1': 0, u'`': 0}
+		u'U': 0, u'I': 0, u'!': 0, u'#': 0, u'%': 0, u'(': 0, u')': 0, u'_': 0, u'+': 0, u'=': 0, u'1': 0, u'`': 0}
 uvwl1 = [u'\u0259', "u", "E", "A", "O", "M", "J", "N", u'i', u'a', "U", u'\u0254', "I", u'!', u'#', u'%', u'(', u')',
-         u'_', u'+', u'=', u'1', u'`']
+		 u'_', u'+', u'=', u'1', u'`']
 long_vowel = ["E", "A", "O", "M", "J", "N", "U", "I"]
 short_vowel = ["i", "o", "u", u'\u0259']
 uc = [u'p', u'b', u't', u'd', u'\u0288', u'\u0256', u't\u0283', u'd\u0292', u'k', u'g', u'q', u'm', u'n', u'\u0273',
-      u'f', u's', u'z', u'\u0283', u'x', u'\u0263', u'h', u'\u028b', u'j', u'r', u'l', u'\u023f', u'\u027d', u'\u0272',
-      u'\u014b']
+	  u'f', u's', u'z', u'\u0283', u'x', u'\u0263', u'h', u'\u028b', u'j', u'r', u'l', u'\u023f', u'\u027d', u'\u0272',
+	  u'\u014b']
 
 ustop = [u'p', u'b', u't', u'k', u'd', u'g', u'P', u'K', u'G', u'D']
 ucp = ["P", "B", "T", "D", "Q", "C", "c", "Z", "5", "K", "R", "G"]
@@ -38,6 +28,7 @@ def Syllabify(entries):
 	temp3=""
 	temp2.append(" ")
 	temp6=[]
+	countv = 0
 	for word in hindi_input_array1: 
 		hindi_output=word
 		hindi_output=Labelchanger1(hindi_output)
@@ -91,7 +82,7 @@ def Syllabify(entries):
 
 	for i in range(0,length_pos):
 		hindi_output1.insert(pos[i],'.')
-	print ("hindi_output after first syll", hindi_output1)
+	#print ("hindi_output after first syll", hindi_output1)
 	for i in range(1,len(hindi_output1)-1):
 		for j in range(0, len(ustop)):
 			if (hindi_output1[i]==ustop[j] and hindi_output1[i+1]=="." and (hindi_output1[i+2]=="r" or hindi_output1[i+2]=="l" or hindi_output1[i+2]==u'\u028b' or hindi_output1[i+2]=="j")):
