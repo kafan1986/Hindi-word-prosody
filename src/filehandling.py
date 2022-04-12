bc95451b1137d36326193328fad8c5d09ec68a17
kafan1986 <debasish1986@gmail.com>
2022-04-13 01:51:15 +0530

    Update filehandling.py

--- src/filehandling.py
+++ src/filehandling.py
@@ -1,22 +1,13 @@
 # -*- coding: utf-8 -*-
 from tkinter import *
-from itertools import groupby
 import tkinter.filedialog
-import codecs
-import sys
-import re
 import os
-import xlsxwriter
 import subprocess
-import xlrd
 import tkinter.font
 #reload(sys)
-from IPAEquv import IPAEquivalent
-from Labelchang1 import Labelchanger1
-from Labelchang2 import Labelchanger2
-from Syllabification import Syllabify
-from Syll_label import Labeling
+from Syll_label import Labeling, TempDICT
 from phoneme import Phoneme
+from tqdm import tqdm
 # reload(sys)
 # sys.setdefaultencoding('utf-8')
 def load_file(entries):
@@ -24,14 +15,20 @@ def load_file(entries):
 	path=os.getcwd()
 	fileoutput=open(path+"/"+"PLS_XML_W3C_Format_Lexicon.txt", 'a')
 	fileoutput1=open(path+"/"+"PLS_Festival_Format_Lexicon.txt", 'a')
-	for word in fileinput:
+	for word in tqdm(fileinput):
 		hindi_input=word.rstrip()
 		entries['Hindi Input'].delete(0,END)
 		entries['Hindi Input'].insert(0,word)
 		Phoneme(entries)
-		fileoutput.write("<lexeme>\n")
-		fileoutput.write("<Grapheme>"+(word).rstrip()+"</Grapheme>"+"\n"+"<PLSB>"+ entries['Prosodic Label(PLSB)'].get() +"</PLSB>"+'\n'+ "<Phoneme>"+entries['Phoneme Level(IPA)'].get()+"</Phoneme>"+'\n' )
-		fileoutput.write("</lexeme>\n")
+		# fileoutput.write("<lexeme>\n")
+		# fileoutput.write("<Grapheme>"+(word).rstrip()+"</Grapheme>"+"\n"+"<PLSB>"+ entries['Prosodic Label(PLSB)'].get() +"</PLSB>"+'\n'+ "<Phoneme>"+entries['Phoneme Level(IPA)'].get()+"</Phoneme>"+'\n' )
+		# fileoutput.write("</lexeme>\n")
 
-		fileoutput1.write("( "+(word).rstrip()+"\t"+entries['Phoneme Level(ASCII)'].get()+" )\n" )
-		subprocess.call(["sed -i 's/\\\t)//g'",path+"/"+"PLS_Festival_Format_Lexicon.txt"], shell=True)
+		# fileoutput1.write("( "+(word).rstrip()+"\t"+entries['Phoneme Level(ASCII)'].get()+" )\n" )
+		# subprocess.call(["sed -i 's/\\\t)//g'",path+"/"+"PLS_Festival_Format_Lexicon.txt"], shell=True)
+	print("--------- TempDICT ---------")
+	tmp = TempDICT()
+	tmp_view = sorted( ((v,k) for k,v in tmp.items()), reverse=True)
+	print("len: ",len(tmp_view))
+	for v,k in tmp_view:
+		print("%s: %d" % (k,v))
